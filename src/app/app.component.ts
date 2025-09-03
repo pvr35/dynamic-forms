9e1fe5b4fe655ef5ed13f19482545303850f44af
Patricio <patriciovado1331@gmail.com>
2025-09-03 08:27:36 -0600

    createproductform en appcomponet ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { ReactiveFormsModule } from '@angular/forms';
+import { Component, inject } from '@angular/core';
+import { Form, FormArray, FormControl, FormGroup, NonNullableFormBuilder, ReactiveFormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
@@ -10,6 +10,23 @@ interface Product {
   name:string;
   price: number;
 }
+
+
+
+
+
+
+
+type ProductForm = FormGroup<{
+  product: FormControl<Product | undefined>;
+  price: FormControl<number>;
+  quantity: FormControl<number>;
+}>;
+
+type form = FormGroup<{
+  client: FormControl<string>;
+  products:FormArray<ProductForm>;
+}>;
 
 @Component({
   selector: 'app-root',
@@ -31,4 +48,19 @@ export class AppComponent {
     { id: 3, name: 'Carrot', price: 12 },
     { id: 4, name: 'Apple', price: 11 },
   ];
+
+  formBuilder = inject(NonNullableFormBuilder);
+
+  form: FormGroup = this.formBuilder.group({
+    client: this.formBuilder.control(''),
+    products: this.formBuilder.array<ProductForm>([]),
+  });
+
+  createProductForm(): ProductForm {
+    return this.formBuilder.group({
+      product: this.formBuilder.control<Product | undefined>(undefined),
+      price: this.formBuilder.control<number>(0),
+      quantity: this.formBuilder.control<number>(0),
+    });
+  }
 }
