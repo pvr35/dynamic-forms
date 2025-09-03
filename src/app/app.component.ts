0580e37fc0b03e0204a2efe80317768a073166c6
Patricio <patriciovado1331@gmail.com>
2025-09-02 20:01:01 -0600

    completar el app componet ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,34 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select'
+
+interface Product {
+  id: number;
+  name:string;
+  price: number;
+}
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    ReactiveFormsModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSelectModule,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'dynamic-forms';
+  products: Product[] = [
+    { id: 1, name: 'cucumber', price: 15.5 },
+    { id: 2, name: 'Tomato', price: 10},
+    { id: 3, name: 'Carrot', price: 12 },
+    { id: 4, name: 'Apple', price: 11 },
+  ];
 }
